119093bd61d2255a995b8fa9cb15966ed423df55
Atyapi! <93179190+Atyapi@users.noreply.github.com>
2023-01-19 14:32:36 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,9 @@
 var x = 0;
+var TextImg;
+
+function preload(){
+  TextImg = loadImg('text.svg');
+}
 
 function setup() {
   createCanvas(480, 360);
@@ -9,12 +14,9 @@ function draw() {
   //var timer = millis()
   background(0);
   
-  //円を表示
-  fill(255,0,0);
-  noStroke();
-  ellipse(x, 180, 60, 60);
-  
   x += (240-x)/8;
+  image(TextImg,x,180);
+  
 }
 
 function Easing(Type, from, to, timer1, timer2) {
