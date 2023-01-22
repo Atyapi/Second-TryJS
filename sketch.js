9799bad093702ea61271151e40c6c2fc39374a13
Atyapi! <93179190+Atyapi@users.noreply.github.com>
2023-01-22 19:18:29 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 var x = 0;
 var y = 0;
+var timer = 0;
 var TextImg;
 
 function preload(){
-  TextImg = loadImg('text.svg');
+  TextImg = loadImg('text.png');
 }
 
 function setup() {
@@ -11,16 +12,10 @@ function setup() {
 }
 
 function draw() {
-  
-  //var timer = millis()
   background(0);
   
   x += (240-x)/8;
   image(TextImg,x,180);
-  
-  fill(255,0,0);
-  ellipse(240, 180, 50, 50);
-  
 }
 
 function Easing(Type, from, to, timer1, timer2) {
